e06add9981f19476741505985d3a248cca50e8e4
Cloy Monis <cloy.m@vmax.com>
2022-01-12 12:37:18 +0530

    jenkins app center integration

--- NGPocDemo/ViewController.swift
+++ NGPocDemo/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterCrashes
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        AppCenter.start(withAppSecret: "a87c626c-67d2-4ae0-80fa-240fb8897a36", services:[
+          Crashes.self
+        ])
     }
-
-
+    
+    @IBAction func actionButton(_ sender: Any) {
+        print("NGPocDemo")
+    }
+    
 }
 
