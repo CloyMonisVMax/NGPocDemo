4d607936102ab56f2bc76e69aed076e750df4054
Cloy Monis <cloy.m@vmax.com>
2022-01-12 17:38:39 +0530

    updated build version to 7

--- NGPocDemo/ViewController.swift
+++ NGPocDemo/ViewController.swift
@@ -11,12 +11,18 @@ import AppCenterCrashes
 
 class ViewController: UIViewController {
 
+    @IBOutlet var btn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         AppCenter.start(withAppSecret: "a87c626c-67d2-4ae0-80fa-240fb8897a36", services:[
           Crashes.self
         ])
+        if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String{
+            btn.titleLabel?.text = "Click to action \(appVersion)(\(buildNumber))"
+        }
+        
     }
     
     @IBAction func actionButton(_ sender: Any) {
